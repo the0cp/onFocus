823f84c17e8e7b55c1eb144645080f414f7050f6
Theodore Cooper <63190431+the0cp@users.noreply.github.com>
2024-12-31 14:59:34 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 // ==UserScript==
-// @name          onFocus
+// @name          AlwaysForcus - AllowCopy - DisableFullscreen
 // @include       *
-// @description  kill blur eventListener, force onfocus
-// @author       the0cp
-// @version      1.0.0
-// @license      GPLv3
 // ==/UserScript==
 
 
@@ -14,6 +10,7 @@ unsafeWindow.blurred = false;
 unsafeWindow.document.hasFocus = () => true;
 unsafeWindow.window.onFocus = () => true;
 
+// kill dom property names
 [
     "hidden",
     "mozHidden",
@@ -28,6 +25,8 @@ Object.defineProperty(document, "webkitVisibilityState", {get: () => "visible"})
 
 unsafeWindow.document.onvisibilitychange = undefined;
 
+
+// element constructors to allow blur events on
 const blurWhitelist = [
     HTMLInputElement,
     HTMLAnchorElement,
@@ -35,19 +34,24 @@ const blurWhitelist = [
     HTMLParagraphElement,
 ]
 
+// element constructors to block mouseleave and mouseout events on
 const hoverBlacklist = [
     HTMLIFrameElement,
     HTMLHtmlElement,
     HTMLBodyElement,
     HTMLHeadElement,
+    HTMLFrameSetElement, // obsolete but included for completeness
+    HTMLFrameElement // obsolete but included for completeness
 ];
 
 var event_handler = (event) => {
+    // if the event is blur, and the target is an whitelisted type, allow it
     if (event.type === 'blur' &&
         ((blurWhitelist.some(type => event.target instanceof type) ||
-            event.target.classList?.contains('ql-editor')))) {
+            event.target.classList?.contains('ql-editor')))) { // quill js fix
         return;
     }
+    // if the event is mouseleave or mouseout, and the target is an blacklisted type, block it
     if (['mouseleave', 'mouseout'].includes(event.type) &&
         !hoverBlacklist.some(type => event.target instanceof type)) {
         return;
@@ -72,3 +76,68 @@ var event_handler = (event) => {
     window.addEventListener(event_name, event_handler, true);
     document.addEventListener(event_name, event_handler, true);
 })
+
+function removeCopyEvent() {
+    Array.prototype.concat.call(document.querySelectorAll('*'), document)
+        .forEach(function (ele) {
+            ele.oncopy = undefined;
+            ele.oncontextmenu = undefined;
+        });
+}
+
+'use strict';
+~function (global) {
+    function generate() {
+        return function () {
+
+            global.addEventListener('contextmenu', function (e) {
+                removeCopyEvent();
+            });
+
+            global.addEventListener('keydown', function (e) {
+                if (e.ctrlKey || e.keyCode === 224 || e.keyCode === 17 || e.keyCode === 91 || e.keyCode === 93) {
+                    removeCopyEvent();
+                }
+            });
+
+            this.hookBefore(EventTarget.prototype, 'addEventListener',
+                function (m, args) {
+                    if (args[0] === 'copy' || args[0] === 'contextmenu') {
+                        args[0] = 'prevent ' + args[0] + ' handler';
+                        console.log('AllowCopy', args[0]);
+                    }
+                }, false);
+
+            var style = 'body * :not(input):not(textarea){-webkit-user-select:auto!important;-moz-user-select:auto!important;-ms-user-select:auto!important;user-select:auto!important}';
+
+            var stylenode = document.createElement('style');
+
+            stylenode.setAttribute("type", "text/css");
+            if (stylenode.styleSheet) {// IE
+                stylenode.styleSheet.cssText = style;
+            } else {// w3c
+                var cssText = document.createTextNode(style);
+                stylenode.appendChild(cssText);
+            }
+
+            document.addEventListener('readystatechange', function () {
+                if (document.readyState === "interactive" || document.readyState === "complete") {
+                    setTimeout(function () {
+                        document.head.appendChild(stylenode);
+                        console.log('AllowCopy css applied')
+                    }, 2000);
+                }
+            });
+
+            console.log('AllowCopy', 'works.');
+        }
+    }
+
+
+    if (global.eHook) {
+        global.eHook.plugins({
+            name: 'allowCopy',
+            mount: generate()
+        });
+    }
+}(window);
